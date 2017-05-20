d2d6d04bfd15b1cc1f5b3ad235954ad642a1d605
Boat <boat46450@hotmail.com>
2017-05-21 01:08:03 +0700

    test bootstrap can't use container and row

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,115 +2,122 @@ var position = '#home';
 var product = ["#ssgs8","#ip7","#ipse","#hp9","#np1"];
 
 $(document).ready(function(){
-  $('.bssgs8').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#ssgs8').css({top: '0'});
-    $('#ssgs8').addClass('front');
-    $('#ssgs8').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bssgs8').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#ssgs8';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#ssgs8').addClass('front');
+        $('#ssgs8').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#ssgs8';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bip7').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#ip7').css({top: '0'});
-    $('#ip7').addClass('front');
-    $('#ip7').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bip7').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#ip7';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#ip7').addClass('front');
+        $('#ip7').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#ip7';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bipse').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#ipse').css({top: '0'});
-    $('#ipse').addClass('front');
-    $('#ipse').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bipse').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#ipse';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#ipse').addClass('front');
+        $('#ipse').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#ipse';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bhp9').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#hp9').css({top: '0'});
-    $('#hp9').addClass('front');
-    $('#hp9').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bhp9').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#hp9';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#hp9').addClass('front');
+        $('#hp9').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#hp9';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bnp1').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#np1').css({top: '0'});
-    $('#np1').addClass('front');
-    $('#np1').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bnp1').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#np1';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#np1').addClass('front');
+        $('#np1').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#np1';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bhome').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#home').css({top: '0'});
-    $('#home').addClass('front');
-    $('#home').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bhome').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#home';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#home').addClass('front');
+        $('#home').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#home';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 
-  $('.bcart').click(function (e) {
-    $(position).css({top: '-100%'});
-    $('#cart').css({top: '0'});
-    $('#cart').addClass('front');
-    $('#cart').removeClass('back');
-    setTimeout(function(){
-        $(position).removeClass('transition');
-        $(position).addClass('back');
+    $('.bcart').click(function (e) {
+        $(position).addClass('flow');
         $(position).removeClass('front');
-        $(position).css({top: '100%'});
-        $(position).addClass('transition');
-        position = '#cart';
-    },2000);
-    e.stopPropagation();
-  });
+        $('#cart').addClass('front');
+        $('#cart').removeClass('back');
+        setTimeout(function(){
+            $(position).removeClass('transition');
+            $(position).addClass('back');
+            $(position).removeClass('flow');
+            setTimeout(function(){
+                $(position).addClass('transition');
+                position = '#cart';
+            }, 10);
+        },2000);
+        e.stopPropagation();
+    });
 });
